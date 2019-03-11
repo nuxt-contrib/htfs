9c55bc94666055716f20c8a2aa680334adaeee93
pooya parsa <pooya@pi0.ir>
2019-03-11 17:30:03 +0330

    100% coverage

---
+++ test/client.test.js
@@ -0,0 +1,11 @@
+const HTTPFSAClient = require('../lib/client')
+
+it('Throws without passing endpoint', () => {
+  expect(() => new HTTPFSAClient()).toThrow('Missing required option `endpoint`')
+})
+
+it('Throws on unexpected status code', async () => {
+  const client = new HTTPFSAClient({ endpoint: 'http://' })
+  client._request = () => Promise.resolve({ status: 500 })
+  await expect(client.exists('/')).rejects.toThrow('Error: Unexpected status 500')
+})
