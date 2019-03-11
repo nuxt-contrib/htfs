b7b7557be4268c916c3050980cf0520b3146dedf
pooya parsa <pooya@pi0.ir>
2019-03-11 17:31:00 +0330

    fix test

--- test/client.test.js
+++ test/client.test.js
@@ -7,5 +7,5 @@ it('Throws without passing endpoint', () => {
 it('Throws on unexpected status code', async () => {
   const client = new HTTPFSAClient({ endpoint: 'http://' })
   client._request = () => Promise.resolve({ status: 500 })
-  await expect(client.exists('/')).rejects.toThrow('Error: Unexpected status 500')
+  await expect(client.exists('/')).rejects.toThrow('Unexpected status 500')
 })
